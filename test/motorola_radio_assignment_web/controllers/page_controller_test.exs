0468d159fe4702ea17f163619f902f3c4d86aca2
Jorge López <jorgeldeducha@gmail.com>
2020-08-16 00:41:08 +0200

    Added test for Scenario 1

--- test/motorola_radio_assignment_web/controllers/page_controller_test.exs
+++ test/motorola_radio_assignment_web/controllers/page_controller_test.exs
@@ -1,8 +1,41 @@
 defmodule MotorolaRadioAssignmentWeb.PageControllerTest do
   use MotorolaRadioAssignmentWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  alias MotorolaRadioAssignment.{Radio, Repo}
+  import Ecto.Query
+
+  def get_max_id() do
+    case Repo.one(from r in Radio, select: max(r.id)) do
+      nil -> 1
+      {id} -> id
+    end
   end
+
+  test "Scenario 1", %{conn: conn} do
+    max_id = get_max_id()
+
+    conn = post(conn, "/radios/#{max_id+1}", ["alias": "Radio100", allowed_locations: ["CPH-1", "CPH-2"]])
+    assert response(conn, 200) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+2}", ["alias": "Radio101", allowed_locations: ["CPH-1", "CPH-2", "CPH-3"]])
+    assert response(conn, 200) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+1}/location", [location: "CPH-1"])
+    assert response(conn, 200) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+2}/location", [location: "CPH-3"])
+    assert response(conn, 200) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+1}/location", [location: "CPH-3"])
+    assert response(conn, 403) =~ ""
+
+    conn = get(conn, "/radios/#{max_id+2}/location")
+    assert json_response(conn, 200)["location"] == "CPH-3"
+
+    conn = get(conn, "/radios/#{max_id+1}/location")
+    assert json_response(conn, 200)["location"] == "CPH-1"
+
+
+  end
+
 end
