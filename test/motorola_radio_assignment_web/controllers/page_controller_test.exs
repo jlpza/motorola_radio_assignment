edd5ad6c5ffd968c99fe5a8c20c456e502fe27f7
Jorge López <jorgeldeducha@gmail.com>
2020-08-16 12:52:41 +0200

    Added tests for posting radios with existing ID and missing data

--- test/motorola_radio_assignment_web/controllers/page_controller_test.exs
+++ test/motorola_radio_assignment_web/controllers/page_controller_test.exs
@@ -46,4 +46,30 @@ defmodule MotorolaRadioAssignmentWeb.PageControllerTest do
     assert response(conn, 404) == ""
   end
 
+  test "Post radio with used ID", %{conn: conn} do
+    max_id = get_max_id()
+
+    conn = post(conn, "/radios/#{max_id+1}", ["alias": "Radio100", allowed_locations: ["CPH-1", "CPH-3"]])
+    assert response(conn, 200) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+1}", ["alias": "Radio100", allowed_locations: ["CPH-1", "CPH-3"]])
+    assert response(conn, 409) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+1}", ["alias": "Radio101", allowed_locations: ["CPH-1", "CPH-2", "CPH-3"]])
+    assert response(conn, 409) =~ ""
+  end
+
+  test "Post radio with missing data", %{conn: conn} do
+    max_id = get_max_id()
+
+    conn = post(conn, "/radios/#{max_id+1}", [allowed_locations: ["CPH-1", "CPH-3"]])
+    assert response(conn, 400) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+2}", ["alias": "Radio100"])
+    assert response(conn, 400) =~ ""
+
+    conn = post(conn, "/radios/#{max_id+3}", [])
+    assert response(conn, 400) =~ ""
+  end
+
 end
